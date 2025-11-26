b920d643591ec428e422895350297f193a194a40
Hamzah Subhani <hamzahsubhani6@gmail.com>
2025-11-26 16:18:17 -0600

    Refactor database connection parameters

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'courses';                // NIU host for MariaDB
-$dbname = 'z2033811';             // <-- replace with member 2's Z-ID database name
-$username = 'z2033811';           // <-- usually same as dbname
-$password = '2006Apr20';      // <-- replace with real password
+$host = 'courses';              
+$dbname = 'z2033811';            
+$username = 'z2033811';        
+$password = '2006Apr20';      
 
 $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
